98766465fe8a689e926236da2452a822551daeea
Anand Doshi <anand.doshi.8@gmail.com>
2021-05-02 17:52:23 +0530

    A lot of changes.

--- functionality.js
+++ functionality.js
@@ -10,7 +10,7 @@ let colourClasses = ["solid-bg", "accent-color-green"];
 
 let accentColour = "accent-color";
 
-$(document).ready(function() {
+$(document).ready(function () {
   recolourNavbar();
   uniformTabHeight();
   resize();
@@ -18,49 +18,43 @@ $(document).ready(function() {
   aboutHover("intent");
   checkMobile();
   // carouselUpdate();
-})
+});
 
-var carouselUpdate = function() {
+var carouselUpdate = function () {
   var topCarousel = $("#top-carousel");
-  topCarousel.on("slide.bs.carousel", function() {
+  topCarousel.on("slide.bs.carousel", function () {
     changeAccentColour("accent-color-green");
   });
-  topCarousel.on("slid.bs.carousel", function() {
-    $(".accent-color-green").each(function() {
+  topCarousel.on("slid.bs.carousel", function () {
+    $(".accent-color-green").each(function () {
       $(this).removeClass("accent-color-green");
     });
     console.log("Removed accent colour.");
-  })
+  });
 };
 
-var changeAccentColour = function(colourClass) {
-
+var changeAccentColour = function (colourClass) {
   var accentColourElements = $(".solid-bg");
   // console.log(accentColourElements);
-  accentColourElements.each(function() {
+  accentColourElements.each(function () {
     $(this).addClass(colourClass);
   });
+};
 
-}
-
-var removeClassFromAll = function(className) {
-  $("."+className).each(function() {
+var removeClassFromAll = function (className) {
+  $("." + className).each(function () {
     $(this).removeClass(className);
   });
-}
+};
 
-var getNextColour = function(colour) {
+var getNextColour = function (colour) {};
 
-}
+var getPreviousColour = function (colour) {};
 
-var getPreviousColour = function(colour) {
-
-}
-
-var resize = function() {
-  $(window).resize(function() {
+var resize = function () {
+  $(window).resize(function () {
     // resetting the height in case the dimensions change
-    $(".tab>.card-block").css("min-height", 0)
+    $(".tab>.card-block").css("min-height", 0);
     uniformTabHeight();
     // console.log("resize");
     checkMobile();
@@ -68,9 +62,9 @@ var resize = function() {
     // resetting the tab buttons in the navbar
     $(".navbar-nav").removeClass(accentColour);
   });
-}
+};
 
-var uniformTabHeight = function() {
+var uniformTabHeight = function () {
   var infoTab = $("#info");
   // resetting.
   infoTab.css("min-height", 0);
@@ -79,21 +73,21 @@ var uniformTabHeight = function() {
   var tab = $(".tab>.card-block");
   // making all tabs of uniform height.
   tab.css("min-height", height);
-}
+};
 
-var uniformCardHeight = function(className) {
+var uniformCardHeight = function (className) {
   var cards = $(className);
   // resetting.
   cards.css("min-height", 0);
   var maxHeight = findMaxHeight(cards);
   // console.log(maxHeight);
   cards.css("min-height", maxHeight);
-}
+};
 
-var findMaxHeight = function(elements) {
+var findMaxHeight = function (elements) {
   var maxHeight = -1;
   // console.log(elements);
-  elements.each(function() {
+  elements.each(function () {
     // console.log(this);
     // console.log(i);
     var height = $(this).outerHeight();
@@ -101,12 +95,12 @@ var findMaxHeight = function(elements) {
     if (height > maxHeight) {
       maxHeight = height;
     }
-  })
+  });
   // console.log(maxHeight);
   return maxHeight;
 };
 
-var getThreshold = function() {
+var getThreshold = function () {
   var doc = $(document);
   var win = $(window);
   var nav = $(".navbar");
@@ -136,7 +130,7 @@ var getThreshold = function() {
   return threshold;
 };
 
-var recolourNavbar = function() {
+var recolourNavbar = function () {
   var doc = $(document);
   var nav = $(".navbar");
 
@@ -147,7 +141,7 @@ var recolourNavbar = function() {
     nav.addClass(accentColour);
   }
 
-  doc.scroll(function() {
+  doc.scroll(function () {
     // pixels that are scrolled down.
     var scroll = doc.scrollTop();
     if (scroll > threshold) {
@@ -158,105 +152,104 @@ var recolourNavbar = function() {
       nav.removeClass(accentColour, transitionTime);
     }
     // console.log(scroll - threshold);
-  })
-}
+  });
+};
 
-var isMobile = function() {
-    var width = $(window).width();
-    var height = $(window).height();
-    var ratio = width/height;
+var isMobile = function () {
+  var width = $(window).width();
+  var height = $(window).height();
+  var ratio = width / height;
 
-    // console.log("isMobile: ");
-    if(ratio < 1)
-    {
-      // console.log("true");
-      return true;
+  // console.log("isMobile: ");
+  if (ratio < 1) {
+    // console.log("true");
+    return true;
+  } else {
+    // console.log("false");
+    return false;
+  }
+};
+
+var toMobile = function () {
+  $("body").addClass("mobile");
+  resize();
+  // console.log("to mobile");
+};
+
+var toDesktop = function () {
+  $("body").removeClass("mobile");
+  resize();
+  // console.log("to desktop");
+};
+
+var checkMobile = function () {
+  if (isMobile()) {
+    toMobile();
+  } else {
+    toDesktop();
+  }
+};
+
+var navButtonClick = function () {
+  var navButton = $("#navButton");
+  navButton.click(function () {
+    $(".navbar-nav").addClass(accentColour, transitionTime);
+    // console.log("Class added");
+    var threshold = getThreshold();
+    // console.log(threshold);
+    // if before threshold, hide colouring.
+    if ($(document).scrollTop() < threshold) {
+      $(".navbar").toggleClass(accentColour, transitionTime);
+      //console.log("Class toggled.");
     }
-    else {
-      // console.log("false");
-      return false;
-    }
-  }
+  });
+};
 
-  var toMobile = function() {
-    $("body").addClass("mobile");
-    resize();
-    // console.log("to mobile");
-  };
+var aboutHover = function (section) {
+  var about = $(".about");
+  var ul = createList(section);
+  ul.hide();
+  var aboutCard;
+  $("#" + section)
+    .children(".card-body")
+    .append(ul);
+  about.hover(function () {
+    // id of the hovered upon card
+    var id = $(this).attr("id");
+    // console.log(id);
+    aboutCard = $("#" + id);
 
-  var toDesktop = function() {
-    $("body").removeClass("mobile");
-    resize();
-    // console.log("to desktop");
-  }
+    // carousel of the hovered on card.
+    var carousel = $("#" + id + "-carousel");
+    // hiding in when hovered.
+    carousel.toggleClass("display-none");
 
-  var checkMobile = function() {
-    if (isMobile()) {
-      toMobile();
-    } else {
-      toDesktop();
-    }
-  };
+    // stretching the card to take up the whole row.
+    aboutCard.toggleClass("col-lg-12");
+    // console.log("col-12");
+    // aligning the text in the card to top right.
+    var cardBody = aboutCard.children(".card-body");
+    cardBody.toggleClass("align-top-left");
 
-  var navButtonClick = function() {
-    var navButton = $("#navButton");
-    navButton.click(function() {
-      $(".navbar-nav").addClass(accentColour, transitionTime);
-      // console.log("Class added");
-      var threshold = getThreshold();
-      // console.log(threshold);
-      // if before threshold, hide colouring.
-      if ($(document).scrollTop() < threshold) {
-        $(".navbar").toggleClass(accentColour, transitionTime);
-        // console.log("Class toggled.");
-      }
-    })
-  }
+    // showing the list only when hovered.
+    $("#" + id + "-list").toggle();
+  });
+};
 
-  var aboutHover = function(section) {
-    var about = $(".about");
-    var ul = createList(section);
-    ul.hide();
-    var aboutCard;
-    $("#" + section).children(".card-body").append(ul);
-    about.hover(function() {
-      // id of the hovered upon card
-      var id = $(this).attr("id");
-      // console.log(id);
-      aboutCard = $("#"+id);
+var createList = function (aboutId) {
+  var ul = $("<ul id='" + aboutId + "-list'></ul>");
+  ul.addClass("pad");
+  ul.addClass("about-list");
+  ul.css("text-align", "left");
+  about[aboutId].forEach((item, i) => {
+    ul.append($("<li></li>").append(item));
+  });
+  return ul;
+};
 
-      // carousel of the hovered on card.
-      var carousel = $("#"+id+"-carousel");
-      // hiding in when hovered.
-      carousel.toggleClass("display-none");
-
-      // stretching the card to take up the whole row.
-      aboutCard.toggleClass("col-lg-12");
-      // console.log("col-12");
-      // aligning the text in the card to top right.
-      var cardBody = aboutCard.children(".card-body");
-      cardBody.toggleClass("align-top-left");
-
-      // showing the list only when hovered.
-      $("#"+id+"-list").toggle();
-    });
-  };
-
-  var createList = function(aboutId) {
-    var ul = $("<ul id='"+aboutId+"-list'></ul>");
-    ul.addClass("pad");
-    ul.addClass("about-list");
-    ul.css("text-align", "left");
-    about[aboutId].forEach((item, i) => {
-      ul.append($("<li></li>").append(item));
-    });
-    return ul;
-
-  }
-
-  var getCurrentItemTopCarousel = function() {
-    // active is the current item displayed.
-    var active = $("#top-carousel>.carousel-inner>.active");
-    // console.log(active.text());
-    return active;
-  }
+var getCurrentItemTopCarousel = function () {
+  // active is the current item displayed.
+  var active = $("#top-carousel>.carousel-inner>.active");
+  // console.log(active.text());
+  return active;
+};
--- update-template.js
+++ update-template.js
@@ -1,52 +1,48 @@
 let weeklyUpdate = {
   week: 52,
   date: "12/12/2020",
-  file: "filename"
+  file: "filename",
 };
 
 let quarterlyUpdate = {
   date: "11/12/2020",
   quarter: 2,
-  file: "filename"
+  file: "filename",
 };
 
 let annualUpdate = {
   year: 2020,
-  file: "filename"
+  file: "filename",
 };
 
 let updateList = [weeklyUpdate, quarterlyUpdate, annualUpdate];
 
-$(document).ready(function() {
+$(document).ready(function () {});
 
-});
+var populateUpdates = function () {};
 
-var populateUpdates = function() {
-
-};
-
-var getUpdateLineTemplate = function() {
+var getUpdateLineTemplate = function () {
   var anchor = "<a></a>";
   anchor.attr("target", "_blank");
   anchor.addClass("update-line");
   return anchor;
 };
 
-var addWeeklyUpdateContent = function(anchor, update) {
+var addWeeklyUpdateContent = function (anchor, update) {
   anchor.append("Week #" + update.week + "; Year: " + getYear(update.date));
   return anchor;
 };
 
-var addQuarterlyUpdateContent = function(anchor, update) {
+var addQuarterlyUpdateContent = function (anchor, update) {
   anchor.append("Date: " + update.date + "; Quarter: " + update.quarter);
   return anchor;
-}
+};
 
-var addAnnualUpdateContent(anchor, update) {
+var addAnnualUpdateContent = function (anchor, update) {
   anchor.append("Year: " + getYear(update.date));
   return anchor;
-}
+};
 
-var getYear = function(date) {
+var getYear = function (date) {
   return date.substr(date.lastIndexOf("/"), 4);
-}
+};
